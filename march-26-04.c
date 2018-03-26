7e30ab8c2d9c1833f88450beaa9fb8747c9aec89
Reazul Islam <reazulislam137@gmail.com>
2018-03-26 22:01:49 +0600

    first commit

---
+++ march-26-04.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void newFunctionOne (void)
+{
+    puts ("Here is the message returning from newFunctionOne.");
+}
+void newFunctionTwo (void)
+{
+    puts ("Here is the message returning from newFunctionTwo.");
+}
+
+int main()
+{
+    atexit (newFunctionOne);
+    atexit (newFunctionTwo);
+
+    puts ("\n This is the message from main function.");
+
+    return 0;
+}
+
